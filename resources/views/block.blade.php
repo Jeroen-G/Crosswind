f62e82b7f2aab7f12cbacbaf0e2d2fab6c37c10a
Jeroen <jeroengjeroeng@gmail.com>
2018-07-02 19:50:33 +0200

    Blade components, WIP

---
+++ resources/views/block.blade.php
@@ -0,0 +1,4 @@
+<section class="sm:w-full md:w-1/4 mr-6 mb-6 rounded-lg overflow-hidden shadow bg-white p-6">
+    <h3 class="font-semibold text-xl mb-4">{{ $title }}</h3>
+    {{ $slot }}
+</section>
